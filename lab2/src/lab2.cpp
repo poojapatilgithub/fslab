980736635b3e30223813fa6f09b756838838fd87
poojapatil <poja13is@gmail.com>
2016-02-22 14:50:37 +0530

    first commit

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -6,10 +6,63 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+#include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
+class Student
+{
+	   string name;
+       string usn;
+       string branch;
+       string sem;
+public:
+       void pack();
+       void unpack();
+       void modify();
+	   void search();
+	   void del();
+	   void read();
+};
+int main()
+{
+	int ch;
+	fstream f,filename;
+	Student s;
+	cout<<"1.insert 2.search 3.delete.4.modify";
+    cout<<"enter the choice";
+	cin>>ch;
+	while(1)
+	{
+		switch(ch)
+		{
+			case 1:
+				s.read();
+				s.pack();
+				break;
+			case 2:
+				//s.search();
+				break;
+			case 3:
+				//s.modify();
+				break;
+			case 4:
+				//s.del();
+				break;
+		}
+	}
+}
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+void Student::read()
+{
+	cout<<"enter name"<<endl;
+	cin>>name;
+	cout<<"enter usn"<<endl;
+	cin>>usn;
+	cout<<"enter branch"<<endl;
+	cin>>branch;
+	cout<<"enter sem"<<endl;
+	cin>>sem;
+}
+}
 }
