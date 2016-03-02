11d2958056ebfa40e1054a3e7ab4341f2d89c21f
poojapatil <poja13is@gmail.com>
2016-03-02 13:05:54 +0530

    first commit

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -1,10 +1,4 @@
-//============================================================================
-// Name        : lab2.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+
 
 #include<iostream>
 #include<fstream>
@@ -19,55 +13,107 @@ class Student
 public:
        void pack();
        void unpack();
-       void modify();
-	   void search();
-	   void del();
-	   void read();
-};
+       void modi
+}#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Student {
+    string usn;
+    string  name;
+    string branch;
+    string sem;
+public:
+    void read();
+    void pack(string filename);
+    void search(string,string);
+    void unpack(string);
+    };
 int main()
 {
-	int ch;
-	fstream f,filename;
-	Student s;
-	cout<<"1.insert 2.search 3.delete.4.modify";
-    cout<<"enter the choice";
-	cin>>ch;
-	while(1)
-	{
-		switch(ch)
-		{
-			case 1:
-				s.read();
-				s.pack();
-				break;
-			case 2:
-				//s.search();
-				break;
-			case 3:
-				//s.modify();
-				break;
-			case 4:
-				//s.del();
-				break;
-		}
-	}
-}
+    int ch;
+    //fstream f;
+    Student s;
+    string fname,key;
+    while(1)
+    {
+    cout<<"1.insert 2.search 3.delete 4.modify\n";
+    cout<<"enter your choice";
+    cin>>ch;
 
-void Student::read()
+         switch(ch)
+        {
+        case 1:
+            s.read();
+            cout<<"enter filename ";
+            cin >> fname;
+            s.pack(fname);
+            break;
+        case 2:
+            cout<<"Enter the USN to be searched\n";
+            cin>>key;
+            s.search(key,fname);
+            break;
+        case 3:
+            //s.modify;
+            break;
+        case 4:
+            //s.unpack();
+            break;
+
+            }
+        }
+}
+void Student::read(){
+    cout<<"enter name"<<endl;
+    cin>>name;
+    cout<<"enter usn"<<endl;
+    cin>>usn;
+    cout<<"enter branch"<<endl;
+    cin>>branch;
+    cout<<"enter sem"<<endl;
+    cin>>sem;
+}
+void Student::pack(string filename){
+    string buffer;
+    ofstream myf(filename.c_str(),ios::app);
+    buffer= usn+"|"+name+"|"+sem+"|"+branch;
+    myf<<buffer<<endl;
+    myf.close();
+
+}
+void Student::unpack(string fname)
 {
-	cout<<"enter name"<<endl;
-	cin>>name;
-	cout<<"enter usn"<<endl;
-	cin>>usn;
-	cout<<"enter branch"<<endl;
-	cin>>branch;
-	cout<<"enter sem"<<endl;
-	cin>>sem;
+    ifstream myf(fname.c_str());
+    getline(myf,usn,'|');
+    getline(myf,name,'|');
+    getline(myf,branch,'|');
+    getline(myf,sem,'|');
+    myf.close();
+
 }
-}
-}
-void Student::pack()
+void Student::search(string key,string fname)
 {
-	string temp,1;
-	temp=usn+|
+    Student s[10];
+    int i=0,count=0;
+
+    ifstream myf(fname.c_str());
+    //cout<<myf;
+    while(!myf.eof())
+    {
+    cout<<"In search\n";
+    s[i].unpack(fname);
+    if(key==s[i].usn)
+    {
+                cout<<"record found\n";
+                cout<<s[i].usn<<"\t"<<s[i].name;
+                break;
+    }
+    i++;
+    }
+
+
+
+    myf.close();
 }
